53b4e2d86322571968155c9579556ef8fb4e5c91
Saily J <30509037+ShellySea@users.noreply.github.com>
2021-10-02 00:38:26 +0530

    useLocation Hook in React

--- src/App.js
+++ src/App.js
@@ -11,14 +11,16 @@ function App() {
       <NoteState>
         <Router>
           <Navbar appName='iNoteBook' />
-          <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route exact path="/about">
-              <About />
-            </Route>
-          </Switch>
+          <div className="container">
+            <Switch>
+              <Route exact path="/">
+                <Home />
+              </Route>
+              <Route exact path="/about">
+                <About />
+              </Route>
+            </Switch>
+          </div>
         </Router>
       </NoteState>
     </div>
--- src/components/About.js
+++ src/components/About.js
@@ -1,17 +1,10 @@
-import React, { useContext, useEffect } from 'react'
-import NoteContext from '../context/notes/noteContext'
+import React from 'react';
 
 function About() {
-    const a = useContext(NoteContext);
-
-    useEffect(() => {
-        a.updateObj();
-        // eslint-disable-next-line
-    }, [])
 
     return (
         <div>
-            This is About Component.Name is {a.obj.name} age is {a.obj.age}
+            This is About Component.
         </div>
     )
 }
